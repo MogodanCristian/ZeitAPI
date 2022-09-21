423090efa01937e15458c3ed21ded5a137b9845f
MogodanCristian <79975756+MogodanCristian@users.noreply.github.com>
2022-09-21 18:28:01 +0300

    User model and register route done

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser')
+//import routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT,
+    {useNewUrlParser: true},
+    () =>{
+        console.log('Connected to DB')
+        })
+
+//Middlewares
+app.use(bodyparser.json());
+
+//route middlewares
+app.use('/api/user', authRoute)
+
 
 app.listen(3000, () =>
 {
-    console.log('Hello World');
+    console.log('Server running');
 })
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+router.post('/register', async (req,res) => {
+    const user = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+router.post('/login')
+
+module.exports = router;
